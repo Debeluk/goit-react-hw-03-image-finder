59b4b52617cf68554b0ce519d4ffdf2b545c907f
Debeluk <117806927+Debeluk@users.noreply.github.com>
2023-12-22 15:28:27 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PhoneBook from './Book/Phonebook';
+
+function App() {
+  return (
+    <div className="App">
+      <PhoneBook />
+    </div>
+  );
+}
+
+export default App;
