5d541cea013ed8961ea50f27f70443ea886d784c
Debeluk <popcorn.starinets@gmail.com>
2023-12-23 09:51:34 +0200

    fixes

--- src/components/ScalePhoto/Modal.js
+++ src/components/ScalePhoto/Modal.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 
 const Modal = ({ image, onClose }) => {
-  const handleOverlayClick = (e) => {
+  const handleOverlayClick = e => {
     if (e.target === e.currentTarget) {
       onClose();
     }
   };
+
+  const handleKeyDown = useCallback(
+    e => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
   return (
     <div className="Overlay" onClick={handleOverlayClick}>
